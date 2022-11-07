6290ea0cb44e07edc23807c63b9ba80d9eb7bac7
CathEvt <katya.evtyh@gmail.com>
2022-11-07 17:14:41 +0300

    Create

---
+++ Dom_Triangle.py
@@ -0,0 +1,7 @@
+a = float(input( "a = " ))
+b = float(input( "b = " ))
+c = float(input( "c = " ))
+if (a+b)>c and (a+c)>b and (b+c)>a:
+    print("треугольник существует")
+else:
+    print("треугольника не сущестует")
